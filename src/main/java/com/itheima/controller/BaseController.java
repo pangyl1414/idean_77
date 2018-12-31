59c83f4ab21f3e45c26b1d05b98839ead9f5c7c9
庞彦龙 <1135734203@qq.com>
2018-12-31 18:48:26 +0800

    右侧的提交

--- src/main/java/com/itheima/controller/BaseController.java
+++ src/main/java/com/itheima/controller/BaseController.java
@@ -1,4 +1,8 @@
 package com.itheima.controller;
 
 public class BaseController {
+    public static void main(String[] args) {
+        System.out.println("rrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+    }
+
 }
